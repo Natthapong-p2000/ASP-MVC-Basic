8a1c62d3637dfffb48dd0672a2c163048ebfd33d
Natthapong-p2000 <natthapong.office@gmail.com>
2024-11-13 12:55:07 +0700

    edit Index

--- ASP-MVC-Basic/Controllers/StudentController.cs
+++ ASP-MVC-Basic/Controllers/StudentController.cs
@@ -14,11 +14,8 @@ namespace ASP_MVC_Basic.Controllers
         }
         public IActionResult Index()
         {
-            Student s1 = new Student();
-            s1.Student_Id = "1";
-            s1.Name = "test";
-            s1.Score = 1;
-            return View();
+            IEnumerable<Student> allStudent = _db.Students;
+            return View(allStudent);
         }
         public IActionResult Create()
         {
