df6ad198347fbd24c520e51673dfaeeb09911439
Natthapong-p2000 <natthapong.office@gmail.com>
2024-11-13 12:46:51 +0700

    add Create form + insert

--- ASP-MVC-Basic/Controllers/StudentController.cs
+++ ASP-MVC-Basic/Controllers/StudentController.cs
@@ -1,21 +1,41 @@
 ﻿using ASP_MVC_Basic.Models;
+using BasicASPTutorial.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_MVC_Basic.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDBContext _db;
+
+        public StudentController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             Student s1 = new Student();
             s1.Student_Id = "1";
             s1.Name = "test";
             s1.Score = 1;
-            return View(s1);
+            return View();
         }
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
--- ASP-MVC-Basic/Models/Student.cs
+++ ASP-MVC-Basic/Models/Student.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP_MVC_Basic.Models
 {
@@ -7,12 +8,16 @@ namespace ASP_MVC_Basic.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "กรุณาป้อนรหัสนักเรียน")]
+        [DisplayName("รหัสนักเรียน")]
         public string Student_Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "กรุณาป้อนชื่อ")]
+        [DisplayName("ชื่อ")]
         public string Name { get; set; }
 
+        [DisplayName("คะแนน")]
+        [Range(0, 100, ErrorMessage = "กรุณาป้อนคะแนนในช่วง 0-100")]
         public int Score { get; set; }
     }
 }
