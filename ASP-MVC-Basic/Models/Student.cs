015ca38b6b84d1bff1033070356de8bf2f75f47f
Natthapong-p2000 <natthapong.office@gmail.com>
2024-11-13 11:38:19 +0700

    add student model

---
+++ ASP-MVC-Basic/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BasicASPTutorial.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "กรุณาป้อนรหัสนักเรียน")]
+        [DisplayName("รหัสนักเรียน")]
+        public string Student_Id { get; set; }
+
+        [Required(ErrorMessage = "กรุณาป้อนชื่อ")]
+        [DisplayName("ชื่อ")]
+        public string Name { get; set; }
+
+        [DisplayName("คะแนน")]
+        [Range(0, 100, ErrorMessage = "กรุณาป้อนคะแนนในช่วง 0-100")]
+        public int Score { get; set; }
+    }
+}
